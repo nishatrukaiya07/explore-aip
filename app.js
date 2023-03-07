b5960640388d24f83ad06f49cf5f547edb3b3c48
nishatrukaiya07 <nishatrukaiya07@gmail.com>
2023-03-07 20:35:44 +0600

    app

---
+++ app.js
@@ -0,0 +1,8 @@
+function usersData (){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res =>res.json())
+    .then(data => displayUsers(data))
+}
+function displayUsers(data){
+    console.log(data)
+}
