e17f6b17aed72321242807ba638c5bb937b30359
nishatrukaiya07 <nishatrukaiya07@gmail.com>
2023-03-07 18:52:39 +0600

    json

---
+++ json.js
@@ -0,0 +1,6 @@
+const users = {id:1, name:'Mona', job:'student'};
+const stringify = JSON.stringify(users);
+console.log(stringify);
+
+// const usersObj = JSON.parse(users);
+// console.log(usersObj);
